d1e4ebe9aa86668149c7de06cf2edf63325eea9e
leechmirror512 <leechmirror512@gmail.com>
2025-12-12 22:56:15 +0530

    Update config_sample.py

--- config_sample.py
+++ config_sample.py
@@ -1,43 +1,68 @@
+# =======================
 # REQUIRED CONFIG
-BOT_TOKEN = ""
-OWNER_ID = 0
-TELEGRAM_API = 0
-TELEGRAM_HASH = ""
+# =======================
+BOT_TOKEN = "5565454842:AAEEmRcxWY2hcI1Qu5L7zGk-o0Do338LVis"
+OWNER_ID = 1911202099
+TELEGRAM_API = 13981010
+TELEGRAM_HASH = "ee97c02dcfbd07ace16f1edc7a8b5ada"
+
+# =======================
 # OPTIONAL CONFIG
+# =======================
+DOWNLOAD_DIR = "/usr/src/app/downloads/"
 TG_PROXY = {}
 USER_SESSION_STRING = ""
-CMD_SUFFIX = ""
-AUTHORIZED_CHATS = ""
-SUDO_USERS = ""
+CMD_SUFFIX = ""                             # Require restart after changing it while bot running
+AUTHORIZED_CHATS = "-1002462533389"
+SUDO_USERS = "930633374"
 DATABASE_URL = ""
 STATUS_LIMIT = 4
-DEFAULT_UPLOAD = "rc"
-STATUS_UPDATE_INTERVAL = 15
+DEFAULT_UPLOAD = "gd"
+STATUS_UPDATE_INTERVAL = 10
 FILELION_API = ""
 STREAMWISH_API = ""
 EXCLUDED_EXTENSIONS = ""
-INCOMPLETE_TASK_NOTIFIER = False
+INCOMPLETE_TASK_NOTIFIER = True
 YT_DLP_OPTIONS = ""
 USE_SERVICE_ACCOUNTS = False
 NAME_SUBSTITUTE = ""
 FFMPEG_CMDS = {}
 UPLOAD_PATHS = {}
+AUTO_DELETE_MESSAGE_DURATION = 20
+IGNORE_PENDING_REQUESTS = ""
+UPTOBOX_TOKEN = ""
+EXTENSION_FILTER = ""
+YT_DLP_QUALITY = ""
+FSUB_IDS = "-1002462533389"
+BOT_PM = ""
+
+# =======================
 # GDrive Tools
-GDRIVE_ID = ""
-IS_TEAM_DRIVE = False
-STOP_DUPLICATE = False
-INDEX_URL = ""
+# =======================
+GDRIVE_ID = "0ABtiIq2dRqvhUk9PVA"
+IS_TEAM_DRIVE = True
+STOP_DUPLICATE = True
+INDEX_URL = "https://backlever.knighthackers.workers.dev/0:"
+
+# =======================
 # Rclone
+# =======================
 RCLONE_PATH = ""
 RCLONE_FLAGS = ""
 RCLONE_SERVE_URL = ""
 RCLONE_SERVE_PORT = 0
 RCLONE_SERVE_USER = ""
 RCLONE_SERVE_PASS = ""
+
+# =======================
 # JDownloader
+# =======================
 JD_EMAIL = ""
 JD_PASS = ""
-# Sabnzbd
+
+# =======================
+# Sabnzbd / Usenet
+# =======================
 USENET_SERVERS = [
     {
         "name": "main",
@@ -58,45 +83,130 @@ USENET_SERVERS = [
         "priority": 0,
     }
 ]
-# Nzb search
+
+# =======================
+# Nzb Search
+# =======================
 HYDRA_IP = ""
 HYDRA_API_KEY = ""
+
+# =======================
 # Update
-UPSTREAM_REPO = ""
+# =======================
+UPSTREAM_REPO = "https://github.com/anasty17/mirror-leech-telegram-bot"
 UPSTREAM_BRANCH = "master"
+
+# =======================
 # Leech
-LEECH_SPLIT_SIZE = 0
-AS_DOCUMENT = False
-EQUAL_SPLITS = False
-MEDIA_GROUP = False
+# =======================
+LEECH_SPLIT_SIZE = ""
+AS_DOCUMENT = ""
+EQUAL_SPLITS = ""
+MEDIA_GROUP = ""
 USER_TRANSMISSION = False
 HYBRID_LEECH = False
 LEECH_FILENAME_PREFIX = ""
 LEECH_DUMP_CHAT = ""
 THUMBNAIL_LAYOUT = ""
-# qBittorrent/Aria2c
-TORRENT_TIMEOUT = 0
+USER_SESSION_STRING = ""
+LEECH_FILENAME_PERFIX = ""
+DUMP_CHAT = ""
+
+# =======================
+# qBittorrent / Aria2c
+# =======================
+TORRENT_TIMEOUT = ""
 BASE_URL = ""
 BASE_URL_PORT = 0
-WEB_PINCODE = False
-# Queueing system
-QUEUE_ALL = 0
-QUEUE_DOWNLOAD = 0
-QUEUE_UPLOAD = 0
+SERVER_PORT = ""
+WEB_PINCODE = ""
+
+# =======================
+# Queueing System
+# =======================
+QUEUE_ALL = ""
+QUEUE_DOWNLOAD = ""
+QUEUE_UPLOAD = ""
+
+# =======================
+# Limits
+# =======================
+STORAGE_THRESHOLD = ""
+TORRENT_LIMIT = ""
+DIRECT_LIMIT = ""
+YTDLP_LIMIT = ""
+GDRIVE_LIMIT = ""
+MEGA_LIMIT = ""
+LEECH_LIMIT = ""
+MAX_PLAYLIST = ""
+CLONE_LIMIT = ""
+
+# =======================
+# Group Features
+# =======================
+USER_MAX_TASKS = ""
+ENABLE_RATE_LIMIT = ""
+ENABLE_MESSAGE_FILTER = ""
+STOP_DUPLICATE_TASKS = ""
+DISABLE_DRIVE_LINK = ""
+
+# =======================
+# Extra
+# =======================
+SET_COMMANDS = True
+DISABLE_LEECH = ""
+DM_MODE = "all"
+DELETE_LINKS = ""
+LOG_CHAT = ""
+
+# =======================
 # RSS
-RSS_DELAY = 600
+# =======================
+RSS_DELAY = ""
 RSS_CHAT = ""
 RSS_SIZE_LIMIT = 0
+RSS_COMMAND = ""
+RSS_CHAT_ID = ""
+RSS_USER_SESSION_STRING = ""
+
+# =======================
+# Mega
+# =======================
+MEGA_API_KEY = ""
+MEGA_EMAIL_ID = ""
+MEGA_PASSWORD = ""
+
+# =======================
+# GDTOT Cookie
+# =======================
+CRYPT = "STNPOXZkbnJoVkZ3Ky9KL01laVNkMUd0OXRzMzZWOUJKbEN1WGp0TEE2MD0%3D"
+
+# =======================
+# Telegraph
+# =======================
+TITLE_NAME = "Mirror Leech Bot"
+AUTHOR_NAME = "Basava Adarsh"
+AUTHOR_URL = "https://t.me/Adarshbasava"
+COVER_IMAGE = ""
+
+# =======================
+# Buttons
+# =======================
+VIEW_LINK = True
+
+# =======================
 # Torrent Search
+# =======================
 SEARCH_API_LINK = ""
-SEARCH_LIMIT = 0
+SEARCH_LIMIT = ""
 SEARCH_PLUGINS = [
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/rarbg.py",
     "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/piratebay.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/legittorrents.py",
     "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/limetorrents.py",
-    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torlock.py",
     "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torrentscsv.py",
+    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/zooqle.py",
     "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/eztv.py",
-    "https://raw.githubusercontent.com/qbittorrent/search-plugins/master/nova3/engines/torrentproject.py",
     "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/kickass_torrent.py",
     "https://raw.githubusercontent.com/MaurizioRicci/qBittorrent_search_engines/master/yts_am.py",
     "https://raw.githubusercontent.com/MadeOfMagicAndWires/qBit-plugins/master/engines/linuxtracker.py",
@@ -104,8 +214,6 @@ SEARCH_PLUGINS = [
     "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/ettv.py",
     "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/glotorrents.py",
     "https://raw.githubusercontent.com/LightDestory/qBittorrent-Search-Plugins/master/src/engines/thepiratebay.py",
-    "https://raw.githubusercontent.com/v1k45/1337x-qBittorrent-search-plugin/master/leetx.py",
     "https://raw.githubusercontent.com/nindogo/qbtSearchScripts/master/magnetdl.py",
-    "https://raw.githubusercontent.com/msagca/qbittorrent_plugins/main/uniondht.py",
-    "https://raw.githubusercontent.com/khensolomon/leyts/master/yts.py",
+    "https://raw.githubusercontent.com/khensolomon/leyts/master/yts.py"
 ]
